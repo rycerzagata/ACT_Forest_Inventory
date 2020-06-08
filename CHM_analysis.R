262b84141a038615b1140b2c3e5d73d940cec3c7
mararizasentis <59556308+mararizasentis@users.noreply.github.com>
2020-06-08 14:59:17 +0200

    Add files via upload

--- CHM_analysis.R
+++ CHM_analysis.R
@@ -48,9 +48,6 @@ DSM <- resample(DSM_proj, DTM1, method = "bilinear")
 CHM <- DSM - DTM1 - 40.68
 plot(CHM, main="CHM", col=matlab.like2(50), xaxt="n", yaxt="n")
 
-##########################################################################################################
-
-
 # We use the Variable Window Filter (VWF) to detect dominant tree tops. We use a linear function used in 
 # forestry and set the minimum height of trees at 10, but those variables can be modified. 
 # After we plot it to check how the tree tops look like. 
@@ -61,6 +58,25 @@ plot(treetops, col="black", pch = 20, cex=0.5, add=TRUE)
 
 # We check the mean of the height of the detected tree tops 
 mean(treetops$height)
+
+# We compute the function MCWS function that implements the watershed algorithm. In this case, the argument
+# minHeight refers to the lowest expected treetop. The result is a raster where each tree crown is 
+# a unique cell value. 
+crowns <- mcws(treetops = treetops, CHM=CHM, minHeight = 8, verbose=FALSE)
+plot(crowns, main="Detected tree crowns", col=sample(rainbow(50), length(unique(crowns[])),replace=TRUE), 
+     legend=FALSE, xaxt="n", yaxt="n")
+
+# We do the same computation as before but changig the output format to polygons. It takes more processing
+# time but polygons inherit the attributes of treetops as height. Also, crown area is computed for each polygon.
+crownsPoly <- mcws(treetops = treetops, CHM=CHM, minHeight = 8, verbose=FALSE, format="polygons")
+plot(CHM, main="CHM", col=matlab.like2(50), xaxt="n", yaxt="n")
+plot(crownsPoly, border="black", lwd=0.5, add=TRUE)
+
+# Assuming each crown has a roughly circular shape,the crown area is used to compute its average circular diameter.
+crownsPoly[["crownDiameter"]] <- sqrt(crownsPoly$crownArea/pi) *2
+mean(crownsPoly$crownDiameter)
+mean(crownsPoly$crownArea)
+
 
 
 
