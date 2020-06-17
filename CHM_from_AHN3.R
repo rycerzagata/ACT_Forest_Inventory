aa9f5a9455a21437ec261475c9224cc0de43dc19
rycerzagata <59534693+rycerzagata@users.noreply.github.com>
2020-06-17 09:52:16 +0200

    Update CHM_from_AHN3.R

--- CHM_from_AHN3.R
+++ CHM_from_AHN3.R
@@ -22,7 +22,7 @@ library(TreeLS)
 setwd("/Users/marariza/Downloads")
 
 # We load and read the AHN3 file
-AHN3_clip <- "AHN3.laz"
+AHN3_clip <- "/Users/HP/Documents/ACT/R/Data/AHN3_beech.laz"
 AHN3 <- readLAS(AHN3_clip)
 
 # Computing the DSM with the AHN3 dataset
@@ -36,12 +36,13 @@ plot(DTM, main="DTM", col=matlab.like2(50))
 # We compute the CHM and remove one value which is below 0 (-0.005 m)
 CHM <- DSM - DTM
 CHM[CHM<0] <- NA
+CHM <- focal(CHM,w=matrix(1/9, nc=3, nr=3), na.rm=TRUE)
 plot(CHM, main="CHM", col=matlab.like2(50), xaxt="n", yaxt="n")
 
 # We use the Variable Window Filter (VWF) to detect dominant tree tops. We use a linear function used in 
 # forestry and set the minimum height of trees at 10, but those variables can be modified. 
 # After we plot it to check how the tree tops look like. 
-lin <- function(x) {x*0.01+2}
+lin <- function(x) { x * 0.06 + 1 }
 treetops <- vwf(CHM = CHM, winFun = lin, minHeight = 15)
 plot(CHM, main="CHM", col=matlab.like2(50), xaxt="n", yaxt="n")
 plot(treetops, col="black", pch = 20, cex=0.5, add=TRUE)
@@ -52,13 +53,13 @@ mean(treetops$height)
 # We compute the function MCWS function that implements the watershed algorithm. In this case, the argument
 # minHeight refers to the lowest expected treetop. The result is a raster where each tree crown is 
 # a unique cell value. 
-crowns <- mcws(treetops = treetops, CHM=CHM, minHeight = 15, verbose=FALSE)
+crowns <- mcws(treetops = treetops, CHM=CHM, minHeight = 10, verbose=FALSE)
 plot(crowns, main="Detected tree crowns", col=sample(rainbow(50), length(unique(crowns[])),replace=TRUE), 
      legend=FALSE, xaxt="n", yaxt="n")
 
 # We do the same computation as before but changig the output format to polygons. It takes more processing
 # time but polygons inherit the attributes of treetops as height. Also, crown area is computed for each polygon.
-crownsPoly <- mcws(treetops = treetops, CHM=CHM, minHeight = 8, verbose=FALSE, format="polygons")
+crownsPoly <- mcws(treetops = treetops, CHM=CHM, minHeight = 10, verbose=FALSE, format="polygons")
 plot(CHM, main="CHM", col=matlab.like2(50), xaxt="n", yaxt="n")
 plot(crownsPoly, border="black", lwd=0.5, add=TRUE)
 
@@ -69,10 +70,4 @@ mean(crownsPoly$crownArea)
 
 sp_summarise(treetops)
 sp_summarise(crownsPoly, variables=c("crownArea", "height"))
-
-#####################################################################################################
-
-
-
-
 
